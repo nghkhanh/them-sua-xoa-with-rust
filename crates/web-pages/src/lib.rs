68d46ba24670e780c306e8b7a489c53febce6dbf
khanh.huu <khanh.huu@mobifone.vn>
2026-03-10 08:11:02 +0000

    chore: remove web-page

--- crates/web-pages/src/lib.rs
+++
@@ -1,8 +0,0 @@
-mod layout;
-pub mod root;
-use dioxus::prelude::*;
-
-pub fn render(page: Element) -> String {
-    let html = dioxus_ssr::render_element(page);
-    format!("<!DOCTYPE html><html lang='en'>{}</html>", html)
-}
